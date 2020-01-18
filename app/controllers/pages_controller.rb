8f9e5d951df887f89db03936d999118a9969b77f
BingQ <bingbing13@163.com>
2020-01-18 00:07:42 -0800

    add some front-end

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,11 @@ class PagesController < ApplicationController
   def home
     @pokemons = Pokemon.all
   end
+
+  def update
+    # update pokemon
+    @pokemon = Pokemon.first
+    @pokemon.avatar.attach(params[:pokemon][:avatar])
+    redirect_to root_path
+  end
 end
